997167721246da589a0fff5eb93b70a5500539e8
enzoanido <enzoanido2011@gmail.com>
2026-08-20 19:07:21 -0300

    Refactor supabase.js to export URL and key

--- js/supabase.js
+++ js/supabase.js
@@ -1,10 +1,5 @@
-import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
-
 const SUPABASE_URL = "https://yxkeahczlguritsowbvq.supabase.co";
 
 const SUPABASE_KEY = "sb_publishable_kJ8K_79tRYBYBxvoZPkA0Q_ud7Fdo2B";
 
-export const supabase = createClient(
-  SUPABASE_URL,
-  SUPABASE_KEY
-);
+export { SUPABASE_URL, SUPABASE_KEY };
