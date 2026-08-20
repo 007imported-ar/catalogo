2633ab36e591906fb5f9d73b7ae0b7e237e443c2
enzoanido <enzoanido2011@gmail.com>
2026-08-20 19:11:50 -0300

    Update Supabase key and initialize client

--- js/supabase.js
+++ js/supabase.js
@@ -1,5 +1,10 @@
+import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm";
+
 const SUPABASE_URL = "https://yxkeahczlguritsowbvq.supabase.co";
 
-const SUPABASE_KEY = "sb_publishable_kJ8K_79tRYBYBxvoZPkA0Q_ud7Fdo2B";
+const SUPABASE_KEY = "TU_PUBLISHABLE_KEY";
 
-export { SUPABASE_URL, SUPABASE_KEY };
+export const supabase = createClient(
+    SUPABASE_URL,
+    SUPABASE_KEY
+);
