729d0c8a6d1c93e9f26df5e47f5992541f36c0bd
enzoanido <enzoanido2011@gmail.com>
2026-08-20 19:12:15 -0300

    Change Supabase publishable key
    Updated the Supabase key to a new publishable key.

--- js/supabase.js
+++ js/supabase.js
@@ -2,7 +2,7 @@ import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js
 
 const SUPABASE_URL = "https://yxkeahczlguritsowbvq.supabase.co";
 
-const SUPABASE_KEY = "TU_PUBLISHABLE_KEY";
+const SUPABASE_KEY = "sb_publishable_kJ8K_79tRYBYBxvoZPkA0Q_ud7Fdo2B";
 
 export const supabase = createClient(
     SUPABASE_URL,
